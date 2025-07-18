bc22208931753e9f32196fa5afbfd99e262143d9
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-07-18 13:43:38 +0100

    i connected the authorize to the routes

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -17,7 +17,7 @@ const authorize = async (req, res, next) => {
 
         const decoded = jwt.verify(token, JWT_SECRET);
 
-        const user = await User.findByID(decoded.userId);
+        const user = await User.findById(decoded.userId);
 
         if (!user)
             return res.status(401).json({message: 'Unauthorized access, user not found'});
