569006c9a31cb01b35e0d0e863bf5e2245e3949d
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-07-23 16:11:03 +0100

    setting up the workflow with config and controllers

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import connectToMongoDB from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
 
 // error handling middleware
 app.use(errorMiddleware);
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,18 +1,30 @@
 import Subscription from "../models/subscription.model.js"
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
     try{
         const newSubscription = await Subscription.create({
             ...req.body,
             user: req.user._id
-        })
+        });
+
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId :newSubscription.id,
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0,
+        });
 
         res.status(201).json({
-            sussess: true,
+            success: true,
             message: "Subscription created successfully",
-            data: newSubscription
-        })
-
+            data: { newSubscription, workflowRunId }
+        });
     }
     catch (e)
     {
